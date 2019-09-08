d5435b67bcf392d7a7afdd8e2059ffc81bd207fd
LXhbyxiang <2413528465@qq.com>
2019-09-09 07:38:07 +0800

    1

---
+++ pages/benefits/courseinfo.vue
@@ -0,0 +1,158 @@
+<template>
+	<view class="course">
+		<view class="">
+			<image src="../../static/chuke.jpg"></image>
+		</view>
+
+		<view class="course-show">
+			<view class="course-name">
+				<text>某某课程名称</text>
+				<view class="course-content">
+					<view class="course-list">
+						轮播图
+					</view>
+					<view class="course-position">
+						<p>
+							<icon type="success" size="26" />
+							<text>2019年7月16号 至 2019年7月19号</text>
+						</p>
+						<p>
+							<icon type="success" size="26" />
+							<text>北京·九华山庄宾馆楼</text>
+						</p>
+						<p>
+							<icon type="success" size="26" />
+							<text>200人</text>
+						</p>
+						<p>
+							<icon type="success" size="26" />
+							<text>
+								课程简介课程简介课程简介课程简介课程简介课程简介
+								课程简介课程简介课程简介课程简介课程简介课程简介
+								课程简介课程简介课程简介
+							</text>
+						</p>
+					</view>
+				</view>
+			</view>
+			<view class="course-info">
+				<view class="course-title">
+					<view class="line"></view>
+					<text class="text">课程详情</text>
+				</view>
+				<view class="course-word">
+					课程详情
+				</view>
+			</view>
+
+		</view>
+		<view class="couse-money">
+			<text class="new-price">￥ 5800元</text>
+			<text class="old-price">原价: ￥ 12800元</text>
+			<button class="btn" type="primary">立即报名</button>
+		</view>
+		<view class="bottom">
+			<button class="btn" type="primary">报名成功</button>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+	@import "../../common/common.scss";
+
+	.course {
+		background-color: $uni-textbg-color-grey;
+
+		image {
+			width: 100%;
+			height: 340rpx;
+			vertical-align: top;
+		}
+
+		.course-show {
+			padding: 0 40rpx;
+
+			.course-name {
+				margin-bottom: 40rpx;
+				border-radius: $uni-border-radius-base;
+				background-color: #fff;
+
+				.course-content {
+					padding: 10rpx 30rpx 30rpx;
+
+					.course-position {
+						color: $uni-text-color;
+						font-size: $uni-font-size-sm;
+
+						p {
+							padding: 18rpx 0;
+							border-bottom: 1px solid $uni-border-color;
+						}
+					}
+				}
+			}
+		}
+
+		.course-info {
+			margin-bottom: 40rpx;
+			padding: 20rpx 0;
+			background-color: #fff;
+			.course-title{
+				display: flex;
+				align-content: center;
+				padding:10rpx 0  20rpx 28rpx;
+				border-bottom:1px solid $uni-border-color;
+				.line{
+					width: 8rpx;
+					font-size: 32rpx;
+					height: 32rpx;
+					border-radius: 8rpx;
+					background-color: $uni-bg-color ;
+				}
+				.text{
+					margin-left: 10rpx;
+					font-size: 32rpx;
+					line-height: 32rpx;
+					color: $uni-bg-color;
+				}
+			}
+		}
+
+		.couse-money {
+			display: flex;
+			align-items: center;
+			padding: 10rpx 30rpx;
+			margin-bottom: 40rpx;
+			background-color: #fff;
+			.new-price {
+				color: $uni-bg-color;
+			}
+			.old-price {
+				margin: 0 30rpx 0 20rpx;
+				text-decoration:line-through;
+				font-size: $uni-font-size-sm;
+				color: $uni-text-color-grey;
+			}
+			.btn {
+				width: 300rpx;
+				height: 60rpx;
+				font-size: $uni-spacing-row-base;
+				background-color: $uni-bg-color;
+			}
+		}
+
+		.bottom {
+			padding: 40rpx;
+			background-color: #fff;
+
+			.btn {
+				height: 60rpx;
+				font-size: $uni-spacing-row-base;
+				background-color: #09BB07;
+			}
+		}
+	}
+</style>
---
+++ pages/my/orderinfo.vue
@@ -0,0 +1,227 @@
+<template>
+	<view class="order">
+		<view class="header">
+			<text>待付款：订单倒计时30分钟，订单取消</text>
+			<image src="../../static/car.png"></image>
+		</view>
+		<view class="store-name">
+			<view class="store-header">
+				<view class="header-left">
+					<image src="../../static/image_massge_people2.png"></image>
+					<text>某某店铺名称</text>
+				</view>
+				<view class="header-right">
+					<button class="friend-pay btn" type="primary" size="mini">朋友代付</button>
+					<button class="me-pay btn" type="primary" size="mini">立即付款</button>
+				</view>
+			</view>
+			<view class="store-content">
+				<view class="goods-info" v-for="o in 2">
+					<image src=""></image>
+					<view class="goods-src">
+						<p>护肤的第一步就是做好面部清洁工作，敏感肌肤应该选择水润</p>
+						<text>套装护肤+洁面</text>
+					</view>
+					<view class="goods-price">
+						<p>￥99.9</p>
+						<text>x1</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="user-info">
+			<view class="user-word">
+				<image src=""></image>
+				<view class="user-order">
+					<p>
+						<text class="user-name">叶小天</text>
+						<text>1869536545</text>
+					</p>
+					<p>
+						北京市  北京市  朝阳区  花园路甲22号建国路甲13号215室
+					</p>
+				</view>
+			</view>
+			<image class="bg-bottom" src="../../static/add_bg.jpg"></image>
+		</view>
+		<view class="order-info">
+			<p class="to-pay">
+				<text>待付金额:</text>
+				<text class="money">￥19.9</text>
+			</p>
+			<p>
+				<text>待赠金币:</text>
+				<text>100个金币</text>
+			</p>
+			<p>
+				<text>订单编号:</text>
+				<text>201803150002625365445</text>
+			</p>
+			<p>
+				<text>付款时间:</text>
+				<text>2018-07-15 15:30:23</text>
+			</p>
+			<p>
+				<text>发货时间:</text>
+				<text>2018-07-15 15:30:23</text>
+			</p>
+		</view>
+		<view class="bottom-line">-- 我是有底线的卡瑞塔 --</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+	.order {
+		background-color: #f4f4f4;
+		.header {
+			display: flex;
+			align-items: center;
+			justify-content: space-around;
+			height: 200rpx;
+			font-size: 32rpx;
+			color: #fff;
+			background: linear-gradient(to right, #ff0180 0%, #ff8175 100%);
+			image {
+				width: 120rpx;
+				height: 80rpx;
+			}
+		}
+
+		.store-name {
+			margin: 20rpx 0;
+			background-color: $uni-text-color-inverse;
+			.store-header {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				padding: 20rpx;
+				border-bottom: 1px solid #f4f4f4;
+				.header-left {
+					display: flex;
+					align-items: center;
+					image {
+						width: 50rpx;
+						height: 50rpx;
+						border-radius: 50%;
+					}
+					text {
+						margin: 0 10rpx;
+						color: $uni-bg-color;
+					}
+				}
+				.header-right {
+					display:flex;
+					align-items:center;
+					.btn {
+						margin: 6rpx;
+						padding: 0 8rpx;
+						height: 50rpx;
+						width: 160rpx;
+						line-height: 50rpx;
+						border-radius: 30rpx;
+					}
+					.friend-pay {
+						background-color: #4aa3f0;						
+					}
+					.me-pay {
+						background-color:$uni-bg-color;
+					}
+				}
+			}
+			.store-content {
+				padding-bottom: 20rpx;
+				.goods-info {
+					display: flex;
+					justify-content: space-between;
+					padding: 40rpx 0;
+					margin: 0 40rpx;
+					border-bottom: 1px solid #f4f4f4;
+					image {
+						width: 140rpx;
+						height: 140rpx;
+						border-radius: 10rpx;
+						background-color: red;
+					}
+					.goods-src {
+						display: flex;
+						flex-wrap: wrap;
+						margin-left: 20rpx;
+						width: 450rpx;
+						letter-spacing: 3rpx;
+						text {
+							margin-top: 30rpx;
+						}
+					}
+					.goods-price {
+						text-align: right;
+					}
+					text {
+						font-size: $uni-font-size-sm;
+						color: $uni-text-color-grey;
+					}
+				}
+			}
+		}
+		
+		.user-info {
+			background-color: $uni-text-color-inverse;
+			
+			.user-word {
+				display: flex;
+				align-items: center;;
+				padding: 20rpx 60rpx 0rpx 30rpx;
+				color: $uni-text-color-grey;
+				image {
+					width: 50rpx;
+					height: 50rpx;
+				}
+				.user-order {
+					margin-left: 30rpx;
+					letter-spacing: 3rpx;
+					p {
+						margin: 10rpx 0;
+						.user-name {
+							margin-right: 40rpx;
+							font-size: $uni-font-size-lg;
+							font-weight: bold;
+						}
+						
+					}
+				}
+			}
+			
+			.bg-bottom {
+				width: 100%;
+				height: 8rpx;
+			}
+		}
+	
+		.order-info {
+			margin: 20rpx 0;
+			padding: 40rpx;
+			background-color: $uni-text-color-inverse;
+			p {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				margin-bottom: 10rpx;
+				color: $uni-text-color-grey;
+			}
+			.to-pay {
+				color: #000;
+			}
+			.money {
+				color: $uni-bg-color;
+			}
+		}
+		
+		.bottom-line {
+			padding-bottom: 40rpx;
+			color: $uni-text-color-grey;
+			text-align: center;
+		}
+	}
+</style>
