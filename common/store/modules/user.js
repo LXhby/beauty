66c50daf501a3515eacc75f8324f072df548a4e6
lxandhby <lxandhby@lxandhbydeMacBook-Pro.local>
2019-09-24 23:10:08 +0800

    0

---
+++ common/store/modules/user.js
@@ -0,0 +1,29 @@
+const state = {
+    userInfo: {
+        headimgurl: "http://thirdwx.qlogo.cn/mmopen/vi_32/oSFOkm2TX3cIepOvPI9Ox6pFXhUvicsC5CTagypSPwBqRLEOzzXL3Iqvfh2jrS9FlG2ydpgQ5J7MaRObdxTk9hA/132",
+        id: 1
+    },
+    token: '0ab538e92c6db78d10b1694e645f3625'
+}
+
+const mutations = {
+    setUserInfo: (state, info) => {
+        console.log('setUserInfo', info);
+        state.userInfo = info
+    },
+    setToken: (state, token) => {
+        console.log('setToken', token);
+        state.token = token
+    },
+}
+
+const actions = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+}
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App'
-
+import store from './common/store/index.js'
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
+	store,
     ...App
 })
 app.$mount()
